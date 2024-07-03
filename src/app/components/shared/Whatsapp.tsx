efeca02b107177c657eaa58d91f16ed3b6f6af5c
max2556 <46759632+max2556@users.noreply.github.com>
2024-07-03 21:24:46 +0300

    Add: Whatsapp component

---
+++ src/app/components/shared/Whatsapp.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { fetchContacts } from "@/app/services/contacts";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+export default function Whatsapp() {
+  const [phoneNumber, setPhoneNumber] = useState<Nillable<string>>(null);
+
+  useEffect(() => {
+    const fetchPhone = async () => {
+      const data = await fetchContacts("phone");
+      if ("value" in data && data.value) setPhoneNumber(data.value.split("+")[1]);
+    };
+    fetchPhone();
+  });
+
+  return (
+    <a href={`https://wa.me/${phoneNumber}`} target="_blank" className="group">
+      <Image
+        src="/assets/images/icon-whatsapp.png"
+        alt="Whatsapp Icon"
+        width={24}
+        height={24}
+        className="group-hover:opacity-80 transition-all duration-200"
+      />
+    </a>
+  );
+}
