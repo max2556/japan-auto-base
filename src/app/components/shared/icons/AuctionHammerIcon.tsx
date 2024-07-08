4419e40b9d32b25e5c91f852c59655cf69a91ae2
max2556 <46759632+max2556@users.noreply.github.com>
2024-07-08 15:57:50 +0300

    Changed CarProps interface. Moved CarInfo icons in separate components

---
+++ src/app/components/shared/icons/AuctionHammerIcon.tsx
@@ -0,0 +1,58 @@
+export function AuctionHammerIcon() {
+  return (
+    <svg
+      width="21"
+      height="20"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="shrink-0"
+    >
+      <rect width="20" height="20" transform="translate(0.5)" fill="white" />
+      <rect x="3.5" y="16" width="8" height="2" rx="1" fill="#0B1736" />
+      <rect
+        x="11.9141"
+        y="9"
+        width="8"
+        height="2"
+        rx="1"
+        transform="rotate(45 11.9141 9)"
+        fill="#0B1736"
+      />
+      <rect
+        x="4.91406"
+        y="8"
+        width="8"
+        height="2"
+        rx="1"
+        transform="rotate(45 4.91406 8)"
+        fill="#0B1736"
+      />
+      <rect
+        x="10.9141"
+        y="2"
+        width="8"
+        height="2"
+        rx="1"
+        transform="rotate(45 10.9141 2)"
+        fill="#0B1736"
+      />
+      <rect
+        x="5.5"
+        y="9.65686"
+        width="8"
+        height="5"
+        transform="rotate(-45 5.5 9.65686)"
+        fill="#0B1736"
+      />
+      <rect
+        x="13.5"
+        y="14.5355"
+        width="5"
+        height="1"
+        transform="rotate(-45 13.5 14.5355)"
+        fill="white"
+      />
+    </svg>
+  );
+}
