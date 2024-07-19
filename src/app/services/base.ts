4bf7c1ffeb6ee7a0164647b2bbcf271c18e08eb9
max2556 <46759632+max2556@users.noreply.github.com>
2024-07-19 18:31:59 +0300

    Created BaseEntity interface

---
+++ src/app/services/base.ts
@@ -0,0 +1,5 @@
+export interface BaseEntity {
+  id: number;
+  created_at: string; // ISO date format: 2022-01-01T00:00:00.000Z
+  updated_at: string; // ISO date format: 2022-01-01T00:00:00.000Z
+}
