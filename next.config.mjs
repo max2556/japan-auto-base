4f468ffbbaf2e8baf6e1e2b4c6153bd5f8b29a70
max2556 <46759632+max2556@users.noreply.github.com>
2024-07-20 19:26:18 +0300

    Fix config for next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: process.env.NEXT_PUBLIC_BASE_URL_IP,
+            port: process.env.NEXT_PUBLIC_BASE_URL_PORT,
+            pathname: '/files/**',
+          },
+          {
+            protocol: 'http',
+            hostname: '45.92.177.64',
+            port: '8103',
+            pathname: '/files/**',
+          },
+        ],
+      },
 };
 
 export default nextConfig;
