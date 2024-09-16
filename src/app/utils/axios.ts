0ddf2347db0adaca6cc6000007b72dd7a9a55d7e
max2556 <46759632+max2556@users.noreply.github.com>
2024-09-16 20:02:38 +0300

    axios baseUrl depends on NODE_ENV

--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -1,6 +1,9 @@
 import axios, { CreateAxiosDefaults } from "axios";
 
-export const baseURL = '/api';
+export const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "/api"
+    : `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_BASE_URL_IP}/api`;
 
 export const createAxiosInstance = () => {
   const axiosConfiguration: CreateAxiosDefaults = {
