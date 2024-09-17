0d7393d46fdee676afb47e6883a56c429ac1d98c
max2556 <46759632+max2556@users.noreply.github.com>
2024-09-17 17:53:47 +0300

    Button now can be Link

--- src/app/components/shared/Button.tsx
+++ src/app/components/shared/Button.tsx
@@ -1,5 +1,6 @@
-import className from 'classnames';
-import { ReactNode } from 'react';
+import className from "classnames";
+import Link from "next/link";
+import { ReactNode } from "react";
 
 type ButtonProps = {
   children: string | ReactNode;
@@ -8,6 +9,7 @@ type ButtonProps = {
   red?: boolean;
   blue?: boolean;
   white?: boolean;
+  link?: string;
 };
 
 const Button: React.FC<ButtonProps> = ({
@@ -16,16 +18,23 @@ const Button: React.FC<ButtonProps> = ({
   red,
   blue,
   white,
+  link,
   ...rest
 }) => {
-  const classes = className(rest.className, 'text-center', {
-    'h-12 lg:h-8 font-bold text-sm bg-brand-red text-white font-sansation rounded-7 lg:rounded-5 hover:bg-brand-dark transition-all duration-200 px-4 lg:px-3':
+  const classes = className(rest.className, "text-center", {
+    "h-12 lg:h-8 font-bold text-sm bg-brand-red text-white font-sansation rounded-7 lg:rounded-5 hover:bg-brand-dark transition-all duration-200 px-4 lg:px-3":
       red,
-    'h-12 lg:h-8 font-bold text-sm bg-brand-dark text-white font-sansation rounded-7 lg:rounded-5 hover:bg-brand-red transition-all duration-200 px-4 lg:px-3':
+    "h-12 lg:h-8 font-bold text-sm bg-brand-dark text-white font-sansation rounded-7 lg:rounded-5 hover:bg-brand-red transition-all duration-200 px-4 lg:px-3":
       blue,
-    'h-12 lg:h-8 font-bold text-sm bg-white text-brand-dark font-sansation rounded-7 lg:rounded-5 hover:bg-brand-dark hover:text-white transition-all duration-200 px-4 lg:px-3':
+    "h-12 lg:h-8 font-bold text-sm bg-white text-brand-dark font-sansation rounded-7 lg:rounded-5 hover:bg-brand-dark hover:text-white transition-all duration-200 px-4 lg:px-3":
       white,
   });
+
+  if (link) return (
+    <Link {...rest} href={link} className={classes + " grid place-content-center"} target="_blank">
+      {children}
+    </Link>
+  )
 
   return (
     <button {...rest} className={classes} onClick={onClick}>
