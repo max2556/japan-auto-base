3d4fe3c91e5e4d3e737cf05d8d9995dfde1f8fbd
preisy <preus_3@mail.ru>
2024-10-03 22:32:09 +0700

    edit 1

--- src/app/components/shared/Logo.tsx
+++ src/app/components/shared/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="h-12 w-fit relative z-10 mx-auto group flex items-center gap-2 bg-white text-sm font-bold rounded-full mt-12 px-4">
-      <Image src="/assets/favicon.svg" alt="logo" width={16}height={16}/>
+      <Image src="/assets/favicon.svg" alt="logo" width={42} height={42}/>
       <span>JapanAutoBase</span>
     </div>
   );
